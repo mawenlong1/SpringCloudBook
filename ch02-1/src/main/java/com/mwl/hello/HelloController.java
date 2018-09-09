515626d4049e90c00de7144b7a88a401423cedf5
mawenlong <15061886113@163.com>
2018-09-10 00:26:36 +0800

    服务提供者改造

--- ch02-1/src/main/java/com/mwl/hello/HelloController.java
+++ ch02-1/src/main/java/com/mwl/hello/HelloController.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -41,5 +39,20 @@ public class HelloController {
     @RequestMapping("getInfo")
     public String info() {
         return "desc:" + desc;
+    }
+
+    @RequestMapping(value = "hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello" + name;
+    }
+
+    @RequestMapping(value = "hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "hello " + user.getName() + "," + user.getAge();
     }
 }
