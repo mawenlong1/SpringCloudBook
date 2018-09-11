d03b91f8fe330d2cbfa64e6b45c89d4677c25c1c
mawenlong <15061886113@163.com>
2018-09-12 00:17:11 +0800

    构建网关

---
+++ ch07-api-gateway/src/main/java/com/mwl/gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.mwl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author mawenlong
+ * @date 2018/9/12
+ * describe:
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApiGatewayApplication.class).web(true).run(args);
+    }
+}
