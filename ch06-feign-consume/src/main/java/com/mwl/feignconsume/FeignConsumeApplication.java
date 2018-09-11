9d6648ea0a03c92ee32e4298b07b52998d53ffa0
mawenlong <15061886113@163.com>
2018-09-11 23:42:09 +0800

    日志配置，主要四种级别：NONE,BASIC(仅记录请求方法、url以及响应状态和执行时间),HEADERS(BASIC+请求和响应头信息),FULL

--- ch06-feign-consume/src/main/java/com/mwl/feignconsume/FeignConsumeApplication.java
+++ ch06-feign-consume/src/main/java/com/mwl/feignconsume/FeignConsumeApplication.java
@@ -1,9 +1,11 @@
 package com.mwl.feignconsume;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author mawenlong
@@ -14,6 +16,11 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignConsumeApplication {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FeignConsumeApplication.class, args);
